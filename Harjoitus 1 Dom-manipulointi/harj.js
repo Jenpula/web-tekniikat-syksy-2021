57b2582874ab87fa84f9edf154c6581d1dc26457
Annala <n1anje00@students.oamk.fi>
2021-11-22 16:27:44 +0200

    Tehty tehtävä b

--- Harjoitus 1 Dom-manipulointi/harj.js
+++ Harjoitus 1 Dom-manipulointi/harj.js
@@ -1,3 +1,7 @@
 //Harj a
 var aelem =  document.getElementById("content")
 console.log(aelem.nodeName)
+
+//Harj b
+var firstBasic = document.querySelector(".basic")
+console.log(firstBasic.nodeName)
