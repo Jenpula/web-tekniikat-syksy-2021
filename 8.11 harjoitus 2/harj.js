573d0710ae435e3d1ed97e9d0488afcb8702d751
Annala <n1anje00@students.oamk.fi>
2021-11-22 15:51:49 +0200

    Tehty tehtävä 5

--- 8.11 harjoitus 2/harj.js
+++ 8.11 harjoitus 2/harj.js
@@ -23,3 +23,33 @@ for(let i = 1; i < 6; i++) {
     li.textContent = "Number " + i;
     ul.appendChild(li)
 }
+
+// Construct next table in the end of div that has id=“content”
+let table = document.createElement("table");
+let thead = document.createElement("thead");
+let tbody = document.createElement("tbody");
+
+table.appendChild(thead);
+table.appendChild(tbody);
+
+document.getElementById("content").appendChild(table);
+
+let rivi1 = document.createElement("tr");
+let otsikko1 = document.createElement("th");
+otsikko1.innerHTML = "Last Name";
+let otsikko2 = document.createElement("th");
+otsikko2.innerHTML = "First Name";
+
+rivi1.appendChild(otsikko1);
+rivi1.appendChild(otsikko2);
+thead.appendChild(rivi1);
+
+let rivi2 = document.createElement("tr");
+let rivi2_data1 = document.createElement("td")
+rivi2_data1.innerHTML = "Doe";
+let rivi2_data2 = document.createElement("td");
+rivi2_data2.innerHTML = "John";
+
+rivi2.appendChild(rivi2_data1);
+rivi2.appendChild(rivi2_data2);
+tbody.appendChild(rivi2);
