0a0316a9538c79d9762ac6df36ca6ded3dfaa3c3
Annala <n1anje00@students.oamk.fi>
2021-11-22 15:49:50 +0200

    Tehty tehtävä 2

--- 8.11 harjoitus 2/harj.js
+++ 8.11 harjoitus 2/harj.js
@@ -1,3 +1,7 @@
 // Changed h2 text to let's manipulate some DOM again.
 let firstChange = document.querySelector("h2");
 firstChange.textContent = "Let´s manipulate some DOM again"
+
+// made an horizontal line above header.
+let line = document.createElement("hr")
+firstChange.parentElement.insertBefore(line, firstChange);
