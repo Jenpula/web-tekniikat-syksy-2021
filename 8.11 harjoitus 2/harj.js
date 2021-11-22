1d16840db67d2c54bb4dc12e3186dd9075c45f7c
Annala <n1anje00@students.oamk.fi>
2021-11-22 15:50:21 +0200

    Tehty tehtävä 3

--- 8.11 harjoitus 2/harj.js
+++ 8.11 harjoitus 2/harj.js
@@ -5,3 +5,11 @@ firstChange.textContent = "Let´s manipulate some DOM again"
 // made an horizontal line above header.
 let line = document.createElement("hr")
 firstChange.parentElement.insertBefore(line, firstChange);
+
+// Change text content between the first and last p element inside article
+let article = document.querySelector("article")
+let p1 = article.firstElementChild
+let p2 = article.lastElementChild
+let temp = p1.textContent
+p1.textContent = p2.textContent
+p2.textContent = temp
