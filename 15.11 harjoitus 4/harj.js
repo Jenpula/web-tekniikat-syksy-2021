f89a83a3977c38e017d99ea97da35fdacf2aa577
Annala <n1anje00@students.oamk.fi>
2021-11-22 16:02:19 +0200

    Tehty funktio addNote

--- 15.11 harjoitus 4/harj.js
+++ 15.11 harjoitus 4/harj.js
@@ -1,7 +1,16 @@
 //Lisätään napin kuuntelijafunktio
-document.querySelector("button").addEventListener("click", addNote)
+document.querySelector("button").addEventListener("click", addNote);
 //Haetaan dokumentin elementit
-let body = document.querySelector("body")
-let user = document.getElementById("name")
-let note = document.getElementById("note")
-let checkbox = document.getElementById("important")
+let body = document.querySelector("body");
+let user = document.getElementById("name");
+let note = document.getElementById("note");
+let checkbox = document.getElementById("important");
+
+function addNote(){
+    //Luodaan otsikko johon tulee nimi-kentästä nimi ja lisäksi aikaleima
+    let h3 = document.createElement("h3");
+    h3.textContent = user.value + "  " + new Date().toLocaleString();
+    //Note lisätään tekstinä p-elementtiin textareasta
+    let p = document.createElement("p");
+    p.textContent = note.value
+}
