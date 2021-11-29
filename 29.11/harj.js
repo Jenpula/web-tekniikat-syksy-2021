fa702b793dccbee60675e1c9b783bda2a52a7e74
Annala <n1anje00@students.oamk.fi>
2021-11-29 13:13:21 +0200

    Toinen harjoitus tehtävästä

--- 29.11/harj.js
+++ 29.11/harj.js
@@ -1,7 +1,7 @@
 
 let courses = [];
 document.querySelector("button").addEventListener("click", addCourse);
-
+document.getElementById("show").addEventListener("click", showAll);
 function addCourse(){
     // haetaan tiedot input kentistä
     let cname = document.getElementById("course").value
@@ -13,4 +13,17 @@ function addCourse(){
     }
     // lisätään olio taulukkoon
     courses.push(course)
+    
 }
+
+function showAll() {
+    courses.forEach( course => {
+        let li = document.createElement("li");
+        li.textContent = "Course " + course.course_name + " has " + course.num + " students registered"
+        document.querySelector("ul").appendChild(li);
+    });
+        
+    }
+
+
+
