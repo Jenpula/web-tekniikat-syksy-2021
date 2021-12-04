4a5c49fe9f5c1f7016b800f1d4cad1996d4870da
Annala <n1anje00@students.oamk.fi>
2021-12-04 15:55:21 +0200

    Lisätty kuva etusivulle. kokeiltu eri värejä. tehty napille toiminnallisuus

---
+++ Javascript palautettava tehtävä/java/code.js
@@ -0,0 +1,8 @@
+document.querySelector("button").addEventListener("click", newFunction);
+
+function newFunction() {
+    document.getElementById("textArea").innerHTML = " ";
+    document.getElementById("text").innerHTML = "<h3>Tutustumaan palveluihimme pääset ottamalla yhteyttä puhelinnumeroomme, lähettämällä sähköpostia tai tulemaan paikanpäälle. <p>Tarjoamme ensimmäisen kerran ilmaiseksi.!</h3> Ota yhtettä puh: 0505503465 <p> Sähköp. hyväolo@gmail.com<p> Osoite: Jukolankuja 2 90350 Oulu<p>"
+    
+
+}
