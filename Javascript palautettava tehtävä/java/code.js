68fab26a8909dde7608ce158cd3c179b63db4dca
Annala <n1anje00@students.oamk.fi>
2021-12-04 16:38:47 +0200

    Lisätty tekstiä ja muuteltu footeria. tehty lisää nappeja joilla kuuntelija

--- Javascript palautettava tehtävä/java/code.js
+++ Javascript palautettava tehtävä/java/code.js
@@ -1,8 +1,17 @@
-document.querySelector("button").addEventListener("click", newFunction);
+document.querySelector("#navButton").addEventListener("click", newFunction);
 
 function newFunction() {
     document.getElementById("textArea").innerHTML = " ";
     document.getElementById("text").innerHTML = "<h3>Tutustumaan palveluihimme pääset ottamalla yhteyttä puhelinnumeroomme, lähettämällä sähköpostia tai tulemaan paikanpäälle. <p>Tarjoamme ensimmäisen kerran ilmaiseksi.!</h3> Ota yhtettä puh: 0505503465 <p> Sähköp. hyväolo@gmail.com<p> Osoite: Jukolankuja 2 90350 Oulu<p>"
-    
 
 }
+document.querySelector("#positive").addEventListener("click", function() {
+    document.getElementById("feedback").innerHTML = "Hienoa että löysit tarvitsemasi<p> Jaa sivu myös ystävillesi!"
+});
+
+document.querySelector("#negative").addEventListener("click", buttonFunction);
+document.querySelector("#negative2").addEventListener("click", buttonFunction);
+
+function buttonFunction() {
+    document.getElementById("feedback").innerHTML = "Mitä tietoa etsit sivulta? Vastaamalla autat meitä kehittämään sivustoamme!"
+};
