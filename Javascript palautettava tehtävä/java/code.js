1ed0bf2606254609eee384d38707b9b778500351
Annala <n1anje00@students.oamk.fi>
2021-12-21 11:22:18 +0200

    Laitettu kommentteja html ja js

--- Javascript palautettava tehtävä/java/code.js
+++ Javascript palautettava tehtävä/java/code.js
@@ -1,12 +1,12 @@
 
-// Tehty http requesti
+// Made http request
 let xmlhttp = new XMLHttpRequest();
 xmlhttp.open("GET", "https://api.covid19api.com/live/country/finland/status/confirmed", true)
  
-// lähetetty requesti
+// Send request
 xmlhttp.send();
 
-// luodaan event handler
+// Event handler
 xmlhttp.onreadystatechange=function() {
 
     if(xmlhttp.readyState === 4 && xmlhttp.status === 200)
@@ -22,16 +22,19 @@ xmlhttp.onreadystatechange=function() {
 
   }
 
+// Button which shows information if user clicks it.
 document.querySelector("#navButton").addEventListener("click", newFunction);
+
 
 function newFunction() {
     document.getElementById("text").innerHTML = "<h3>Tutustumaan palveluihimme pääset ottamalla yhteyttä puhelinnumeroomme, lähettämällä sähköpostia tai tulemaan paikanpäälle. <p>Tarjoamme ensimmäisen kerran ilmaiseksi.!</h3> <p> Ota yhtettä: <p>puh: 0505503465 <p> Sähköp. hyväolo@gmail.com<p> Osoite: Jukolankuja 2 90350 Oulu<p>"
 
 }
+// Button which shows feedback for user.
 document.querySelector("#positive").addEventListener("click", function() {
     document.getElementById("feedback").innerHTML = "<p>Hienoa että löysit tarvitsemasi<p> Jaa sivu myös ystävillesi!"
 });
-
+// Button which shows feedback input so user can send message.
 document.querySelector("#negative").addEventListener("click", buttonFunction);
 document.querySelector("#negative2").addEventListener("click", buttonFunction);
 
@@ -41,7 +44,7 @@ function buttonFunction() {
 };
 
 
-
+// Shows alert when user send message.
 function sendmsg() {
     alert("Viestisi on lähetetty onnistuneesti!")
     document.getElementById("feedback").innerHTML = "";
